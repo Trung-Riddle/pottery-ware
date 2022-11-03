d219337509dcdf6cf4ffa95a1d840c1e87c94ffc
vicn17 <nhutvichung@gmail.com>
2022-11-04 00:54:32 +0700

    22/11/4 upload view by Vi

---
+++ view/layouts/Header/index.php
@@ -0,0 +1,149 @@
+<header class="header shadow-01">
+    <div class="container">
+        <div class="row-2 v-center">
+            <div class="header-item item-left">
+                <div class="logo">
+                    <a href="#">
+                        <img src="./view/image/amphora.png" alt="pottery ware">
+                        <span>Pottery ware</span>
+                    </a>
+                </div>
+            </div>
+            <!-- menu start here -->
+            <div class="header-item item-center">
+                <div class="menu-overlay">
+                </div>
+                <nav class="menu">
+                    <div class="mobile-menu-head">
+                        <div class="go-back"><i class="fa fa-angle-left"></i></div>
+                        <div class="current-menu-title"></div>
+                        <div class="mobile-menu-close"><i class="fa-light fa-xmark"></i></div>
+                    </div>
+                    <ul class="menu-main">
+                        <li>
+                            <a href="index.html">trang chủ</a>
+                        </li>
+                        <li class="menu-item-has-children">
+                            <a href="#">Giới Thiệu <i class="fa-light fa-chevron-down"></i></a>
+                            <div class="sub-menu mega-menu mega-menu-column-4">
+                                <div class="list-item text-center">
+                                    <a href="#">
+                                        <img src="https://cdn.shopify.com/s/files/1/0616/4833/9114/products/shop04.2.jpg?v=1657181639"
+                                            alt="new Product">
+                                        <h4 class="title">Bình Hoa Antic</h4>
+                                    </a>
+                                </div>
+                                <div class="list-item text-center">
+                                    <a href="#">
+                                        <img
+                                            src="https://cdn.shopify.com/s/files/1/0616/4833/9114/products/shop01.jpg?v=1657276071">
+                                        <h4 class="title">Bình đất sét</h4>
+                                    </a>
+                                </div>
+                                <div class="list-item text-center">
+                                    <a href="#">
+                                        <img src="https://cdn.shopify.com/s/files/1/0616/4833/9114/products/shop01.1.jpg?v=1657276071"
+                                            alt="new Product">
+                                        <h4 class="title">Bình Đất sét</h4>
+                                    </a>
+                                </div>
+                                <div class="list-item text-center">
+                                    <a href="#">
+                                        <img src="https://cdn.shopify.com/s/files/1/0616/4833/9114/products/shop01.2.jpg?v=1657276071"
+                                            alt="new Product">
+                                        <h4 class="title">Bình Đất Sét</h4>
+                                    </a>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="menu-item-has-children">
+                            <a href="#">Sản Phẩm <i class="fa-light fa-chevron-down"></i></a>
+                            <div class="sub-menu mega-menu mega-menu-column-4">
+                                <div class="list-item">
+                                    <h4 class="title">Bát đĩa</h4>
+                                    <ul>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                    </ul>
+                                    <h4 class="title">Bình Hoa</h4>
+                                    <ul>
+                                        <li><a href="#">Tên Sản Phẩm</a></li>
+                                        <li><a href="#">Tên Sản Phẩm</a></li>
+                                        <li><a href="#">Tên Sản Phẩm</a></li>
+                                    </ul>
+                                </div>
+                                <div class="list-item">
+                                    <h4 class="title">Linh vật đất sét</h4>
+                                    <ul>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                    </ul>
+                                    <h4 class="title">Nồi đất sét</h4>
+                                    <ul>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                    </ul>
+                                </div>
+                                <div class="list-item">
+                                    <h4 class="title">Sản phẩm nổi bật</h4>
+                                    <ul>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                        <li><a href="#">Tên sản phẩm</a></li>
+                                    </ul>
+                                </div>
+                                <div class="list-item">
+                                    <img src="https://cdn.shopify.com/s/files/1/0616/4833/9114/files/grid02_d0e5bce2-d241-4460-b467-dad5af99786e.jpg?v=1657170207"
+                                        alt="shop">
+                                </div>
+                            </div>
+                        </li>
+                        <li class="menu-item-has-children">
+                            <a href="#">Tin Tức <i class="fa-light fa-chevron-down"></i></a>
+                            <div class="sub-menu single-column-menu">
+                                <ul>
+                                    <li><a href="#">Tin Mới Nhất</a></li>
+                                    <li><a href="#">Tin Nổi Bật</a></li>
+                                    <li><a href="#">Nghệ Thuật làm gốm</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="menu-item-has-children">
+                            <a href="#">Tài Khoản <i class="fa-light fa-chevron-down"></i></a>
+                            <div class="sub-menu single-column-menu">
+                                <ul>
+                                    <li><a href="#"> <i class="fa-light fa-user"></i>&nbsp; Đăng Nhập</a></li>
+                                    <li><a href="#"><i class="fa-light fa-user-plus"></i>&nbsp; Đăng ký</a></li>
+
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <!-- menu end here -->
+            <div class="header-item item-right">
+                <a href="#"><i class="fa-light fa-magnifying-glass"></i></a>
+                <a href="#"><i class="fa-light fa-heart"></i></a>
+                <a href="#"><i class="fa-light fa-cart-plus"></i></a>
+                <!-- mobile menu trigger -->
+                <div class="mobile-menu-trigger">
+                    <i class="fa-regular fa-grid"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
