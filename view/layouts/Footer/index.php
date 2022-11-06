ee6074e32ddb5c300f49d637afef9585d764aa67
Trung-Riddle <nguyendinhtrung06052001@gmail.com>
2022-11-06 17:09:46 +0700

    fix detail page trung

---
+++ view/layouts/Footer/index.php
@@ -0,0 +1,119 @@
+<footer class="text-center text-lg-start bg-white text-muted">
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <!-- Left -->
+        <div class="me-5 d-none d-lg-block">
+            <span>Xem shop ở các trang mạng xã hội:</span>
+        </div>
+        <!-- Left -->
+
+        <!-- Right -->
+        <div>
+            <a href="" class="me-4 link-secondary">
+                <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="" class="me-4 link-secondary">
+                <i class="fab fa-twitter"></i>
+            </a>
+            <a href="" class="me-4 link-secondary">
+                <i class="fab fa-google"></i>
+            </a>
+            <a href="" class="me-4 link-secondary">
+                <i class="fab fa-instagram"></i>
+            </a>
+            <a href="" class="me-4 link-secondary">
+                <i class="fab fa-linkedin"></i>
+            </a>
+            <a href="" class="me-4 link-secondary">
+                <i class="fab fa-github"></i>
+            </a>
+        </div>
+        <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
+    <section class="">
+        <div class="container text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <!-- Content -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        <img src="./view/image/amphora_icons.png" height="34" alt=""> Pottery Ware
+                    </h6>
+                    <p>
+                        Tên thương hiệu.
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Sản Phẩm
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-reset">Bát đĩa</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Bình đất</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Nồi Đất</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Tên sản phẩm</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Chính sách
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-reset">Thanh toán</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Đổi trả</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Giao hàng</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Vận chuyển</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">Liên Hệ</h6>
+                    <p><i class="fas fa-home me-3 text-secondary"></i> hcm city, hcm 700000, VN</p>
+                    <p>
+                        <i class="fas fa-envelope me-3 text-secondary"></i>
+                        nguyendinhtrung@gmail.com
+                    </p>
+                    <p><i class="fas fa-phone me-3 text-secondary"></i> + 01 234 567 89</p>
+                    <p><i class="fas fa-print me-3 text-secondary"></i> + 01 234 567 89</p>
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.025);">
+        © 2022 Copyright:
+        <a class="text-reset fw-bold" href="">potteryware.com</a>
+    </div>
+    <!-- Copyright -->
+</footer>
--- view/layouts/Login/index.php
+++ view/layouts/Login/index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Tài Khoản pottery-ware</title>
     <link rel="stylesheet" href="../asset/styles/login.css" />
-  </head>
-  <body>
+</head>
+
+<body>
     <div class="container">
         <div class="app front">
             <div class="header">
@@ -19,22 +21,23 @@
             </div>
             <div class="main">
                 <div class="user_field field-2">
-                    <form class="_details details-login">
+                    <form id="formLogin" class="_details details-login" method="post">
                         <div class="textbox">
-                            <input type="text">
+                            <input type="text" required />
                             <span class="input_detail">Tên Tài Khoản Hoặc Email</span>
                         </div>
                         <div class="textbox">
-                            <input type="password">
+                            <input type="password" required />
                             <span class="input_detail">Mật Khẩu</span>
                         </div>
                     </form>
                 </div>
-                <button class="btn login">Đăng Nhập</button>
+                <button form="formLogin" class="btn login" type="submit" name="submitLogin" value="submitLogin">
+                    Đăng Nhập
+                </button>
             </div>
         </div>
-    
-    
+
         <div class="app back">
             <div class="header">
                 <span class="info">Đăng Ký</span>
@@ -45,32 +48,35 @@
             </div>
             <div class="main">
                 <div class="user_field rel">
-                        <div class="avt">
-                            <img class="camera-icon" src="../asset/image/camera.png" alt="camera">
-                            <img class="avt-img" src="../asset/image/avatar-user.png" alt="avatar">
-                            <input type="file" id="up_file">
-                            <small>Chọn Ảnh</small>
-                        </div>
-                    <form class="_details">
+                    <div class="avt">
+                        <img class="camera-icon" src="../asset/image/camera.png" alt="camera" />
+                        <img class="avt-img" src="../asset/image/user.png" alt="avatar" />
+                        <input type="file" id="up_file" />
+                        <small>Chọn Ảnh</small>
+                    </div>
+                    <form id="formSignup" class="_details">
                         <div class="textbox">
-                            <input type="text">
+                            <input type="text" required />
                             <span class="input_detail">Tên Tài Khoản</span>
                         </div>
                         <div class="textbox">
-                            <input type="email">
+                            <input type="text" required />
                             <span class="input_detail">Email</span>
                         </div>
                         <div class="textbox">
-                            <input type="password">
+                            <input type="password" required />
                             <span class="input_detail">Mật Khẩu</span>
                         </div>
                     </form>
                 </div>
-                <button class="btn login">Đăng Ký</button>
+                <button form="formSignup" class="btn login" type="submit" name="submitSignup" value="submitSignup">
+                    Đăng Ký
+                </button>
             </div>
         </div>
     </div>
 
     <script src="./login.js"></script>
-  </body>
+</body>
+
 </html>
