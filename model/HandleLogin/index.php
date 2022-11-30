6b8048ac3c571d7f616dd1588e7ee3eca6a8e994
Trung-Riddle <nguyendinhtrung06052001@gmail.com>
2022-11-30 14:28:44 +0700

    test

--- model/HandleLogin/index.php
+++ model/HandleLogin/index.php
@@ -10,7 +10,7 @@ if (isset($_POST['dangnhap']) && ($_POST['dangnhap'])) {
         $account = selectAllDataDB("SELECT * FROM user INNER JOIN customer ON user.ur_id = customer.cus_id_user WHERE ur_name = '$ur_name' AND ur_pass = '$ur_pass'");
         $idUser = $account[0]['ur_id'];
         $numRandom = rand(1000, 9999);
-        setcookie('ur_id', $numRandom.$idUser, time() + 86400, '/pottery-ware');
+        setcookie('ur_id', $numRandom.$idUser, time() + 86400, '/');
         header("location: ../../index.php");
     } else {
         header("location: ../../view/layouts/Login/index.php?login=FaildLogin");
