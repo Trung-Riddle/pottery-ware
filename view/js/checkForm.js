820e234b3455e8858c64a6d150cb8831077e3e68
vicn17 <nhutvichung@gmail.com>
2022-12-01 20:18:19 +0700

    fix login

--- view/js/checkForm.js
+++ view/js/checkForm.js
@@ -1,37 +1,184 @@
+//todo: box error
+{
+  /* <div class="filterError">
+<div class="boxError">
+    <div class="textError">
+    </div>
+    <a href="" id="backError">Quay lại</a>
+</div>
+</div> */
+}
+
 //* Check form đăng nhập
-document.getElementById("btnSubmitLogin").onclick = (e) => {
-  var userName = document.getElementById("userName");
-  var pass = document.getElementById("pass");
-  var error = false;
-  var checkUserName = "";
-  var checkPass = "";
+if (document.getElementById("btnSubmitLogin")) {
+  document.getElementById("btnSubmitLogin").onclick = (e) => {
+    var userName = document.getElementById("userName");
+    var pass = document.getElementById("pass");
+    var error = false;
+    var checkUserName = "";
+    var checkPass = "";
 
-  //* hàm check lỗi
-  function checkError(textError) {
-    e.preventDefault();
-    var error = document.querySelector(".filterError");
-    var text = `<span>!</span> ${textError} <br /><br />`;
-    error.style.visibility = "visible";
-    setTimeout(() => {
-      error.style.visibility = "hidden";
-    }, 3000);
-    return text;
-  }
+    //* hàm check lỗi
+    function checkError(textError) {
+      e.preventDefault();
+      var error = document.querySelector(".filterError");
+      var text = `<span>!</span> ${textError} <br /><br />`;
+      error.style.visibility = "visible";
+      error.style.opacity = 1;
+      // setTimeout(() => {
+      //   error.style.visibility = "hidden";
+      //   error.style.opacity = 0;
+      // }, 3000);
+      return text;
+    }
 
-  //* Check user name
-  if (userName.value == "") {
-    checkUserName = checkError("Tên đăng nhập không được bỏ trống.");
-    error = true;
-  }
+    //* Check user name
+    if (userName.value == "") {
+      checkUserName = checkError("Tên đăng nhập không được bỏ trống.");
+      error = true;
+    }
 
-  //* Check password
-  if (pass.value == "") {
-    checkPass = checkError("Mật khẩu không được bỏ trống.");
-    error = true;
-  }
+    //* Check password
+    if (pass.value == "") {
+      checkPass = checkError("Mật khẩu không được bỏ trống.");
+      error = true;
+    }
 
-  //* Hiển thị lỗi
-  if (error == true) {
-    document.querySelector(".textError").innerHTML = checkUserName + checkPass;
-  }
+    //* Hiển thị lỗi
+    if (error == true) {
+      document.querySelector(".textError").innerHTML =
+        checkUserName + checkPass;
+    }
+  };
+}
+
+//* Check form đăng ký
+if (document.getElementById("btnSubmitSignUp")) {
+  document.getElementById("btnSubmitSignUp").onclick = (e) => {
+    var userName = document.getElementById("ur_name");
+    var email = document.getElementById("cus_email");
+    var pass = document.getElementById("ur_pass");
+    var forgot_pass = document.getElementById("forgot_pass");
+    var error = false;
+    var checkUserName = "";
+    var checkEmail = "";
+    var checkPass = "";
+    var checkForgotPass = "";
+
+    //* hàm check lỗi
+    function checkError(textError) {
+      e.preventDefault();
+      var error = document.querySelector(".filterError");
+      var text = `<span>!</span> ${textError} <br /><br />`;
+      error.style.visibility = "visible";
+      error.style.opacity = 1;
+      // setTimeout(() => {
+      //   error.style.visibility = "hidden";
+      // }, 3000);
+      return text;
+    }
+
+    //* Check user name
+    if (userName.value == "") {
+      checkUserName = checkError("Tên đăng nhập không được bỏ trống.");
+      error = true;
+    }
+
+    //* Check email
+    if (email.value == "") {
+      checkEmail = checkError("Email không được bỏ trống.");
+      error = true;
+    }
+
+    //* Check password
+    if (pass.value == "") {
+      checkPass = checkError("Mật khẩu không được bỏ trống.");
+      error = true;
+    }
+
+    //* Check confirm password
+    if (forgot_pass.value == "") {
+      checkForgotPass = checkError("Nhập lại mật khẩu không được bỏ trống.");
+      error = true;
+    }
+
+    //* Hiển thị lỗi
+    if (error == true) {
+      document.querySelector(".textError").innerHTML =
+        checkUserName + checkEmail + checkPass + checkForgotPass;
+    }
+  };
+}
+
+//* Check forgot pass
+if (document.getElementById("submitForm")) {
+  document.getElementById("submitForm").onclick = (e) => {
+    var email = document.getElementById("cus_email");
+    if (document.getElementById("code")) {
+      var code = document.getElementById("code");
+      var pass = document.getElementById("ur_pass");
+      var forgot_pass = document.getElementById("forgot_pass");
+    }
+    var error = false;
+    var checkCode = "";
+    var checkEmail = "";
+    var checkPass = "";
+    var checkForgotPass = "";
+
+    //* hàm check lỗi
+    function checkError(textError) {
+      e.preventDefault();
+      var error = document.querySelector(".filterError");
+      var text = `<span>!</span> ${textError} <br /><br />`;
+      error.style.visibility = "visible";
+      error.style.opacity = 1;
+      // setTimeout(() => {
+      //   error.style.visibility = "hidden";
+      // }, 3000);
+      return text;
+    }
+
+    //* Check code
+    if (code && code.value == "") {
+      checkCode = checkError("Mã xác nhận không được bỏ trống.");
+      error = true;
+    }
+
+    //* Check email
+    if (email && email.value == "") {
+      checkEmail = checkError("Email không được bỏ trống.");
+      error = true;
+    }
+
+    //* Check password
+    if (pass && pass.value == "") {
+      checkPass = checkError("Mật khẩu không được bỏ trống.");
+      error = true;
+    }
+
+    //* Check confirm password
+    if (forgot_pass && forgot_pass.value == "") {
+      checkForgotPass = checkError("Nhập lại mật khẩu không được bỏ trống.");
+      error = true;
+    }
+
+    //* Hiển thị lỗi
+    if (error == true) {
+      document.querySelector(".textError").innerHTML =
+        checkCode + checkEmail + checkPass + checkForgotPass;
+    }
+  };
+}
+
+//* Back error
+document.getElementById("backError").onclick = (e) => {
+  e.preventDefault();
+  var fillterError = document.querySelector(".filterError");
+  fillterError.style.opacity = 0;
+  fillterError.style.visibility = "hidden";
+};
+document.querySelector(".filterError").onclick = () => {
+  var fillterError = document.querySelector(".filterError");
+  fillterError.style.opacity = 0;
+  fillterError.style.visibility = "hidden";
 };
--- view/js/main.js
+++ view/js/main.js
@@ -94,3 +94,11 @@ function openTabs(el) {
 
   btnTarget.classList.add("active");
 }
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    document.querySelector(".header").style.position = "fixed";
+  } else {
+    document.querySelector(".header").style.position = "relative";
+  }
+});
